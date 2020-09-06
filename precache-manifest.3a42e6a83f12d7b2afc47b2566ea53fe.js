cb62e687816d22852c83f9522e2cbd9865b32850
Sirarpy <sipadavtyan@gmail.com>
2020-09-06 22:13:39 +0400

    Updates

---
+++ precache-manifest.3a42e6a83f12d7b2afc47b2566ea53fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35f3bf9e77c444af23424cead556777d",
+    "url": "/ReactJs_form/index.html"
+  },
+  {
+    "revision": "f2c1bbedec960a5c0229",
+    "url": "/ReactJs_form/static/css/main.cc8daa60.chunk.css"
+  },
+  {
+    "revision": "5a7a9d2392b38b84fa8d",
+    "url": "/ReactJs_form/static/js/2.a0e9f7e6.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/ReactJs_form/static/js/2.a0e9f7e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2c1bbedec960a5c0229",
+    "url": "/ReactJs_form/static/js/main.5d874cbb.chunk.js"
+  },
+  {
+    "revision": "0d69aa8bae5b8b03f94d",
+    "url": "/ReactJs_form/static/js/runtime-main.d9edddc0.js"
+  }
+]);
